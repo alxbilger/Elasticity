247aafcdbd6852b52e5d675e6886292889fa5aab
Alex Bilger <alexbilger0@gmail.com>
2025-07-20 09:37:48 +0200

    add support of more element types

--- src/Elasticity/LinearSmallStrainFEMForceField.cpp
+++ src/Elasticity/LinearSmallStrainFEMForceField.cpp
@@ -8,8 +8,14 @@ void registerLinearSmallStrainFEMForceField(sofa::core::ObjectFactory* factory)
     factory->registerObjects(sofa::core::ObjectRegistrationData("Hooke's law on linear triangles assuming small strain")
         .add< LinearSmallStrainFEMForceField<sofa::defaulttype::Vec3Types, sofa::geometry::Triangle> >(true));
 
+    factory->registerObjects(sofa::core::ObjectRegistrationData("Hooke's law on linear quads assuming small strain")
+        .add< LinearSmallStrainFEMForceField<sofa::defaulttype::Vec3Types, sofa::geometry::Quad> >(true));
+
     factory->registerObjects(sofa::core::ObjectRegistrationData("Hooke's law on linear tetrahedra assuming small strain")
         .add< LinearSmallStrainFEMForceField<sofa::defaulttype::Vec3Types, sofa::geometry::Tetrahedron> >(true));
+
+    factory->registerObjects(sofa::core::ObjectRegistrationData("Hooke's law on linear hexahedra assuming small strain")
+        .add< LinearSmallStrainFEMForceField<sofa::defaulttype::Vec3Types, sofa::geometry::Hexahedron> >(true));
 }
 
 template class ELASTICITY_API LinearSmallStrainFEMForceField<sofa::defaulttype::Vec1Types, sofa::geometry::Edge>;
