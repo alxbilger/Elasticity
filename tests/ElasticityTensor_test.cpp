37b70ec351a6c866cec7152ab514635a09619e49
Alex Bilger <alexbilger0@gmail.com>
2025-11-12 22:27:41 +0100

    add test on the elasticity tensor data structure

---
+++ tests/ElasticityTensor_test.cpp
@@ -0,0 +1,44 @@
+#include <Elasticity/impl/ElasticityTensor.h>
+#include <gtest/gtest.h>
+
+namespace elasticity
+{
+
+template<class DataTypes>
+void generateThenAccess()
+{
+    constexpr auto spatial_dimensions = DataTypes::spatial_dimensions;
+    const ElasticityTensor<DataTypes> t([](sofa::Index i, sofa::Index j, sofa::Index k, sofa::Index l)
+    {
+        return kroneckerDelta<SReal>(i, j) * kroneckerDelta<SReal>(k, l);
+    });
+
+    for (sofa::Index i = 0; i < spatial_dimensions; ++i)
+    {
+        for (sofa::Index j = 0; j < spatial_dimensions; ++j)
+        {
+            for (sofa::Index k = 0; k < spatial_dimensions; ++k)
+            {
+                for (sofa::Index l = 0; l < spatial_dimensions; ++l)
+                {
+                    EXPECT_DOUBLE_EQ(t(i, j, k, l), static_cast<SReal>(i == j) * static_cast<SReal>(k == l)) << "i = " << i << " j = " << j << " k = " << k << " l = " << l;
+                }
+            }
+        }
+    }
+}
+
+TEST(ElasticityTensor, generateThenAccess1d)
+{
+    generateThenAccess<sofa::defaulttype::Vec1Types>();
+}
+TEST(ElasticityTensor, generateThenAccess2d)
+{
+    generateThenAccess<sofa::defaulttype::Vec2Types>();
+}
+TEST(ElasticityTensor, generateThenAccess3d)
+{
+    generateThenAccess<sofa::defaulttype::Vec3Types>();
+}
+
+}
