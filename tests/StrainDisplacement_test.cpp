bbf96fcbff9fe8fc03b597c025e3458f83f053bd
Alex Bilger <alexbilger0@gmail.com>
2026-01-16 13:53:53 +0100

    valid tests

--- tests/StrainDisplacement_test.cpp
+++ tests/StrainDisplacement_test.cpp
@@ -6,6 +6,14 @@
 TEST(StraintDisplacement, matrixVectorProduct)
 {
     sofa::type::Mat<4, 3, SReal> dN_dq(sofa::type::NOINIT);
+    for (std::size_t i = 0; i < 4; ++i)
+    {
+        for (std::size_t j = 0; j < 3; ++j)
+        {
+            dN_dq(i, j) = (i + 4) * (j + 9);
+        }
+    }
+
     const auto B = elasticity::makeStrainDisplacement<sofa::defaulttype::Vec3Types, sofa::geometry::Tetrahedron>(dN_dq);
 
     sofa::type::Vec<12, SReal> v;
@@ -18,15 +26,23 @@ TEST(StraintDisplacement, matrixVectorProduct)
     const auto Bv = B * v;
     const auto expectedBv = B.B * v;
 
-    for (std::size_t i = 0; i < 12; ++i)
+    for (std::size_t i = 0; i < 6; ++i)
     {
-        EXPECT_DOUBLE_EQ(Bv[i], expectedBv[i]);
+        EXPECT_DOUBLE_EQ(Bv[i], expectedBv[i]) << "i = " << i;
     }
 }
 
 TEST(StraintDisplacement, matrixTransposedVectorProduct)
 {
     sofa::type::Mat<4, 3, SReal> dN_dq(sofa::type::NOINIT);
+    for (std::size_t i = 0; i < 4; ++i)
+    {
+        for (std::size_t j = 0; j < 3; ++j)
+        {
+            dN_dq(i, j) = (i + 4) * (j + 9);
+        }
+    }
+
     const auto B = elasticity::makeStrainDisplacement<sofa::defaulttype::Vec3Types, sofa::geometry::Tetrahedron>(dN_dq);
 
     sofa::type::Vec<6, SReal> v;
@@ -38,8 +54,8 @@ TEST(StraintDisplacement, matrixTransposedVectorProduct)
     const auto B_Tv = B.multTranspose(v);
     const auto expectedB_Tv = B.B.multTranspose(v);
 
-    for (std::size_t i = 0; i < 6; ++i)
+    for (std::size_t i = 0; i < 12; ++i)
     {
-        EXPECT_DOUBLE_EQ(B_Tv[i], expectedB_Tv[i]);
+        EXPECT_DOUBLE_EQ(B_Tv[i], expectedB_Tv[i]) << "i = " << i;
     }
 }
